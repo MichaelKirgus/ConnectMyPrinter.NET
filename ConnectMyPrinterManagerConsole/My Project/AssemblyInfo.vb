08a2c713434fcc7c6bb518d626237554d1623060
Michael Kirgus <michael@kirgus.net>
2019-02-18 22:16:35 +0100

    Hinzufügen der ManagementCLI für Supportadmins sowie für die Integration in andere Anwendungen.

---
+++ ConnectMyPrinterManagerConsole/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("ConnectMyPrinterManagerConsole")>
+<Assembly: AssemblyDescription("Verwaltungstool für Befehlszeilen-Automatisierung")>
+<Assembly: AssemblyCompany("Michael Kirgus")>
+<Assembly: AssemblyProduct("ConnectMyPrinterManagerConsole")>
+<Assembly: AssemblyCopyright("Copyright ©  2019 Michael Kirgus")>
+<Assembly: AssemblyTrademark("ConnectMyPrinter.NET")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("28c73178-bf82-4b48-853e-e5c4fab4b935")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
