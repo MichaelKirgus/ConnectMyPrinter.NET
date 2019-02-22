7b20f4ac80359542abe1b3cae9036f1a52583474
Michael Kirgus <michael@kirgus.net>
2019-02-22 23:21:53 +0100

    Kommentieren der (Remote-)Profildatei sowie das Hinzufügen der Dokumentationsdatei zum Installationspaket

--- ConnectMyPrinterRemoteFileHandler/RemoteFileClass.vb
+++ ConnectMyPrinterRemoteFileHandler/RemoteFileClass.vb
@@ -1,8 +1,26 @@
 ﻿<Serializable> Public Class RemoteFileClass
+    ''' <summary>
+    ''' Definiert eine benutzerdefinierte Einstellungsdatei für die Anwendungseinstellungen. Standardmäßig wird die AppSettings.xml im Anwendungsverzeichnis geladen.
+    ''' </summary>
     Public Property CustomAppSettingsFile As String = ""
+    ''' <summary>
+    ''' Aktionen, welche vor dem Verbinden und Trennen von Druckern ausgeführt werden.
+    ''' </summary>
     Public Property Preactions As New List(Of RemoteFileActions)
+    ''' <summary>
+    ''' Drucker, welche auf dem Client entfernt werden.
+    ''' </summary>
     Public Property DisconnectPrinters As New List(Of RemoteFilePrinterDisconnectItem)
+    ''' <summary>
+    ''' Aktionen, welche nach dem Verbinden, aber vor dem Trennen von Druckern ausgeführt werden.
+    ''' </summary>
     Public Property IntermediateActions As New List(Of RemoteFileActions)
+    ''' <summary>
+    ''' Drucker, welche auf dem Client verbunden werden sollen oder bereits auf dem Client verbunden sind.
+    ''' </summary>
     Public Property ConnectPrinters As New List(Of RemoteFilePrinterConnectItem)
+    ''' <summary>
+    ''' Aktionen, welche nach dem Verbinden und Trennen von Druckern ausgeführt werden.
+    ''' </summary>
     Public Property Postactions As New List(Of RemoteFileActions)
 End Class
