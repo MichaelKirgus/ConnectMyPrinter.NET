409e2ca5ababe8057007a9f1183985acf590e1cb
Michael Kirgus <michael@kirgus.net>
2019-02-24 01:26:50 +0100

    Funktion für das direkte Senden von Mails implementiert

--- ConnectMyPrinterOutlookHelper/OutlookHelperClass.vb
+++ ConnectMyPrinterOutlookHelper/OutlookHelperClass.vb
@@ -1,4 +1,6 @@
-﻿Public Class OutlookHelperClass
+﻿Imports System.Net.Mail
+
+Public Class OutlookHelperClass
     Public Sub SendOutlookMail(ByVal Betreff As String,
                 ByVal Bodytext As String,
                 Optional ByVal Anhang As String = "Datei als Anhang",
@@ -43,6 +45,40 @@
         Catch ex As Exception
             MsgBox(ex.Message, MsgBoxStyle.Exclamation)
         End Try
-
     End Sub
+
+    Public Function SendMailInternal(ByVal SMTPServer As String, ByVal SMTPUsername As String, ByVal SMTPPassword As String, ByVal SMTPPort As Integer, ByVal EnableSMTPSSL As Boolean, ByVal FROMAdress As String, ByVal ToAdress As String, ByVal Subject As String, ByVal IsBodyHTML As Boolean, ByVal Message As String, ByVal AttachmentFile As String) As Boolean
+        Try
+            Dim Smtp_Server As New SmtpClient
+            Dim e_mail As New MailMessage()
+            If SMTPUsername = "" Then
+                Smtp_Server.UseDefaultCredentials = True
+            Else
+                Smtp_Server.UseDefaultCredentials = False
+                Smtp_Server.Credentials = New Net.NetworkCredential(SMTPUsername, SMTPPassword)
+            End If
+
+            Smtp_Server.Port = SMTPPort
+            Smtp_Server.EnableSsl = EnableSMTPSSL
+            Smtp_Server.Host = SMTPServer
+
+            e_mail = New MailMessage()
+            e_mail.From = New MailAddress(FROMAdress)
+
+            If Not AttachmentFile = "" Then
+                Dim att As New Attachment(AttachmentFile)
+                e_mail.Attachments.Add(att)
+            End If
+
+            e_mail.To.Add(ToAdress)
+            e_mail.Subject = Subject
+            e_mail.IsBodyHtml = IsBodyHTML
+            e_mail.Body = Message
+            Smtp_Server.Send(e_mail)
+
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
 End Class
