b02627265d88b8b8e194b6f22bdb7fa3b62b6928
Michael Kirgus <michael@kirgus.net>
2019-04-06 18:08:24 +0200

    Anpassung Assembly-Version Hauptanwendung.

--- ConnectMyPrinter.NET/My Project/AssemblyInfo.vb
+++ ConnectMyPrinter.NET/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.1.0.28")>
-<Assembly: AssemblyFileVersion("1.1.0.28")>
+<Assembly: AssemblyVersion("1.1.0.29")>
+<Assembly: AssemblyFileVersion("1.1.0.29")>
 <Assembly: NeutralResourcesLanguage("de-DE")>
