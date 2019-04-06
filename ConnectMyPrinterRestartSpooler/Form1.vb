216ca274dd82f7fd8ba90de4920ad8bbae9ab04f
Michael Kirgus <michael@kirgus.net>
2019-04-06 18:16:10 +0200

    Hinzufügen des GNU-Lizenzverweises sowie Copyright zu allen Quellcodedateien.

--- ConnectMyPrinterRestartSpooler/Form1.vb
+++ ConnectMyPrinterRestartSpooler/Form1.vb
@@ -1,4 +1,8 @@
-﻿Public Class Form1
+﻿'Copyright (C) 2016-2019 Michael Kirgus
+'This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+'This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+'You should have received a copy of the GNU General Public License along with this program; if not, see <https://www.gnu.org/licenses>.
+Public Class Form1
     Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
         Try
             Dim jj As New ConnectMyPrinterPrinterManageLib.ManagePrinter
--- ConnectMyPrinterTrayApp/OtherMethods.vb
+++ ConnectMyPrinterTrayApp/OtherMethods.vb
@@ -1,4 +1,8 @@
-﻿Imports System.Windows.Forms
+﻿'Copyright (C) 2016-2019 Michael Kirgus
+'This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+'This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+'You should have received a copy of the GNU General Public License along with this program; if not, see <https://www.gnu.org/licenses>.
+Imports System.Windows.Forms
 
 Friend Module OtherMethods
 
